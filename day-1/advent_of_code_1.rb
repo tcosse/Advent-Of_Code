036f2ba06e7ee022b3c49f280130283a1fcb4a74
Thomas Cosse <tomcosse123@gmail.com>
2023-12-11 14:59:24 +0100

    moved things around

--- day-1/advent_of_code_1.rb
+++ day-1/advent_of_code_1.rb
@@ -10,7 +10,7 @@ sum = 0
 file_data.each do |line|
   nums = []
   line.each_char do |char|
-    nums<<char.to_i if char.integer?
+    nums << char.to_i if char.integer?
   end
   line_number = nums[0].to_s + nums[-1].to_s
   sum += line_number.to_i
