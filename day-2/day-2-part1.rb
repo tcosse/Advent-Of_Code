8c25ef11fb49c9c10f2a719ae51d2e2497083495
Thomas Cosse <tomcosse123@gmail.com>
2023-12-11 21:08:23 +0100

    temporary commit - unfinished work

--- day-2/day-2-part1.rb
+++ day-2/day-2-part1.rb
@@ -1,27 +1,34 @@
-file = File.open('day-2-input.txt')
-file_data = file.readlines.map(&:chomp)
+def possible_games(data_array, max_green_balls, max_red_balls, max_blue_balls)
+  data_array.each do |game_array|
+    pick_id = 0
+    if game_array[pick_id][color] > max_green_balls
 
-filtered_data = []
-
-colors = %w[red green blue]
-
-file_data.each do |line|
-  p line
-  line.gsub!(/Game \d+:/, '')
-  p line
-  game = line.split(/;/)
-  p game
-  game.each do |pick|
-    number_of_balls = {}
-    colors.each do |color|
-      if pick.match?(color)
-        number_of_balls[color] = pick[/(\d+) #{color}/, 1]
-      else
-        number_of_balls[color] = 0
-      end
-    end
-    filtered_data << number_of_balls
   end
 end
 
-p filtered_data
+def parsing_data(file_data)
+  parsed_data = []
+  colors = %w[red green blue]
+  file_data.each do |line|
+    p line
+    line.gsub!(/Game \d+:/, '')
+    p line
+    game = line.split(/;/)
+    p game
+    game.each do |pick|
+      number_of_balls = {}
+      colors.each do |color|
+        if pick.match?(color)
+          number_of_balls[color] = pick[/(\d+) #{color}/, 1]
+        else
+          number_of_balls[color] = 0
+        end
+      end
+      parsed_data << number_of_balls
+    end
+  end
+  parsed_data
+end
+
+file = File.open('day-2-input.txt')
+file_data = file.readlines.map(&:chomp)
